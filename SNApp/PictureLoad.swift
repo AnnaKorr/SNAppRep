3cdc54c5bd3f9f7ebd767e580efe9e89444dc968
AnnaKorr <7319162@gmail.com>
2017-07-26 14:23:25 +0300

    26-07-2017 - 1

---
+++ SNApp/PictureLoad.swift
@@ -0,0 +1,9 @@
+//
+//  PictureLoad.swift
+//  SNApp
+//
+//  Created by apple on 26.07.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+import Foundation
--- SNApp/ViewController.swift
+++ SNApp/ViewController.swift
@@ -52,7 +52,6 @@ class ViewController: UITableViewController {
                 let myJSON = JSON(myValue)
                 let info = InfoListRealm()
                 info.articles_name = myJSON["articles"][0]["title"].stringValue
-                self.newArt.articleName = myJSON["articles"][0]["title"].stringValue
 
                 for (_, subJSON) in myJSON["articles"] {
                     let aaa = SNAppData()
@@ -71,7 +70,6 @@ class ViewController: UITableViewController {
                 
                 print("OMG \n***\(info)")
                 
-                load = true as AnyObject
                 try! myRealm.write {
                     myRealm.add(info, update: true)
                 }
@@ -102,84 +100,6 @@ class ViewController: UITableViewController {
         return artclsArray
     }
     
-   /* func loadFile() {
-        let listNews = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=6b7c247d75914da0b7a53c8bb951c279"
-        
-        let myPath = NSHomeDirectory() + "/Documents/testfile2Write"
-        print("Дома \n\(myPath)")
-        let tempPAth = NSTemporaryDirectory()
-        print("На работе \n\(tempPAth)")
-        
-        //создание и запись файла
-        do {
-            try listNews.write(toFile: myPath, atomically: true, encoding: String.Encoding.utf8)
-            print("NOTE: \n***FIle was created")
-        } catch let error as NSError {
-            print("NOTE: \n***File was not created, the reason is \(error).")
-        }
-        
-        //чтение файла
-        var newContent = ""
-        do {
-            newContent = try NSString(contentsOfFile: myPath, encoding: String.Encoding.utf8.rawValue) as String
-            print("NOTE: \n***File content is:\n\(newContent).")
-
-        } catch let error as NSError {
-            print("NOTE: \n***File content is not readable, the reason is \(error).")
-
-        }
-        
-        //скачивание в файл информации по ссылке
-        let destination = DownloadRequest.suggestedDownloadDestination(for: .documentDirectory)
-        Alamofire.download(listNews, to: destination)
-            .downloadProgress { progress in
-            print("Download Progress: \(progress.fractionCompleted)")
-        }
-            .responseData { response in
-                if response.result.value != nil {
-                    print("jij \(String(describing: response.destinationURL?.path)) kokok")
-                }
-        }
-        
-    }*/
-    
-   /* func loadDataBase(articleName: String) -> Results<Dog> {
-        let realmDB = try! Realm()
-        let myData = realmDB.objects(Dog.self).filter("dogName BEGINSWITH %@", articleName)
-        
-        return myData
-    }
-    
-    func removeInfoFromDB(title: String) {
-        let removeRealm = try! Realm()
-        let removeData = removeRealm.objects(Dog.self).filter("dogName BEGINSWITH %@", title)
-        
-        try! removeRealm.write {
-            removeRealm.delete(removeData)
-        }
-    } */
-
-    
-      /* func loadArticlesList() -> [DataManager] {
-        let realmListArticles = try! Realm()
-        var newArticlesList: [DataManager] = []
-        let articlesData = realmListArticles.objects(Dog.self)
-        
-        for da in articlesData {
-            let newInfo: DataManager = DataManager()
-            newInfo.articleName = da.dogName
-            for subDa in da.myList {
-                newInfo.authorList.append(subDa.author)
-                newInfo.titleList.append(subDa.title)
-            }
-            newArticlesList.append(newInfo)
-        }
-        
-        print("Hi: \n\(newArticlesList)")
-
-        return newArticlesList
-        
-    } */
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let aaa = classCityList
@@ -206,6 +126,84 @@ class ViewController: UITableViewController {
 
 }
 
+/* func loadFile() {
+ let listNews = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=6b7c247d75914da0b7a53c8bb951c279"
+ 
+ let myPath = NSHomeDirectory() + "/Documents/testfile2Write"
+ print("Дома \n\(myPath)")
+ let tempPAth = NSTemporaryDirectory()
+ print("На работе \n\(tempPAth)")
+ 
+ //создание и запись файла
+ do {
+ try listNews.write(toFile: myPath, atomically: true, encoding: String.Encoding.utf8)
+ print("NOTE: \n***FIle was created")
+ } catch let error as NSError {
+ print("NOTE: \n***File was not created, the reason is \(error).")
+ }
+ 
+ //чтение файла
+ var newContent = ""
+ do {
+ newContent = try NSString(contentsOfFile: myPath, encoding: String.Encoding.utf8.rawValue) as String
+ print("NOTE: \n***File content is:\n\(newContent).")
+ 
+ } catch let error as NSError {
+ print("NOTE: \n***File content is not readable, the reason is \(error).")
+ 
+ }
+ 
+ //скачивание в файл информации по ссылке
+ let destination = DownloadRequest.suggestedDownloadDestination(for: .documentDirectory)
+ Alamofire.download(listNews, to: destination)
+ .downloadProgress { progress in
+ print("Download Progress: \(progress.fractionCompleted)")
+ }
+ .responseData { response in
+ if response.result.value != nil {
+ print("jij \(String(describing: response.destinationURL?.path)) kokok")
+ }
+ }
+ 
+ }*/
+
+/* func loadDataBase(articleName: String) -> Results<Dog> {
+ let realmDB = try! Realm()
+ let myData = realmDB.objects(Dog.self).filter("dogName BEGINSWITH %@", articleName)
+ 
+ return myData
+ }
+ 
+ func removeInfoFromDB(title: String) {
+ let removeRealm = try! Realm()
+ let removeData = removeRealm.objects(Dog.self).filter("dogName BEGINSWITH %@", title)
+ 
+ try! removeRealm.write {
+ removeRealm.delete(removeData)
+ }
+ } */
+
+
+/* func loadArticlesList() -> [DataManager] {
+ let realmListArticles = try! Realm()
+ var newArticlesList: [DataManager] = []
+ let articlesData = realmListArticles.objects(Dog.self)
+ 
+ for da in articlesData {
+ let newInfo: DataManager = DataManager()
+ newInfo.articleName = da.dogName
+ for subDa in da.myList {
+ newInfo.authorList.append(subDa.author)
+ newInfo.titleList.append(subDa.title)
+ }
+ newArticlesList.append(newInfo)
+ }
+ 
+ print("Hi: \n\(newArticlesList)")
+ 
+ return newArticlesList
+ 
+ } */
 
 
 var load: AnyObject? {
